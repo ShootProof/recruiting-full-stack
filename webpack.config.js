358c126224b0437c8711145c379e3d361f673bd0
Adrian Ziemkowski <adrian@tave.com>
2019-08-27 17:09:38 -0400

    Building the HTML file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
 module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
@@ -10,6 +12,18 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        }
+      },
     ],
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ],
 }
