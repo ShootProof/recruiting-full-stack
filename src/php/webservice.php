26c7a0dd388cc62b1dac5f34d52854025043f4e3
Adrian Ziemkowski <adrian@tave.com>
2019-08-27 17:33:00 -0400

    Adding instructions to the PHP file

--- src/php/webservice.php
+++ src/php/webservice.php
@@ -1,3 +1,17 @@
 <?php
+/*
 
-// TODO
+TODO:
+
+If your system has PHP installed, you can easily test this script by using PHP's
+built-in web server:
+
+$ php -S localhost:8000 -t src/php/
+
+You can then fetch() this script in JavaScript with the following URL:
+http://localhost:8000/webservice.php
+
+*/
+
+
+echo 'Hello';
