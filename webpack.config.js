8a499d3364dddf9bde0712e52a6cfb4784e9bd43
Adrian Ziemkowski <adrian@tave.com>
2019-08-27 16:56:05 -0400

    Switching to Webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+}
